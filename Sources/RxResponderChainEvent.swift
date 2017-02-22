35868dfbbfebcb1d9ac39056a3814d3662c53faf
ukitaka <ukitaka@linecorp.com>
2017-02-22 18:17:17 +0900

    Add RxResponderChainEvent

---
+++ Sources/RxResponderChainEvent.swift
@@ -0,0 +1,12 @@
+//
+//  RxResponderChainEvent.swift
+//  RxResponderChain
+//
+//  Created by Yuki Takahashi on 2017/02/22.
+//  Copyright © 2017年 waft. All rights reserved.
+//
+
+import RxSwift
+
+public protocol RxResponderChainEvent {
+}
