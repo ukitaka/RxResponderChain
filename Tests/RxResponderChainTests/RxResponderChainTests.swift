4028d68a07b6fa1ae8e75d230b3d323126c5f53d
ukitaka <yuki.takahashi.1126@gmail.com>
2017-02-23 10:56:28 +0900

    Add tests

--- Tests/RxResponderChainTests/RxResponderChainTests.swift
+++ Tests/RxResponderChainTests/RxResponderChainTests.swift
@@ -24,13 +24,23 @@ class RxResponderChainTests: XCTestCase {
         }
     }
 
+    struct TestEvent2: ResponderChainEvent, Equatable {
+        let a: Int
+
+        static func == (lhs: TestEvent2, rhs: TestEvent2) -> Bool {
+            return lhs.a == rhs.a
+        }
+    }
+
     let disposeBag = DisposeBag()
 
     func testResponderChainEvent() {
         let viewController = UIViewController()
         let view = viewController.view!
-        let subview = UIView()
-        view.addSubview(subview)
+        let subview1 = UIView()
+        let subview2 = UIView()
+        view.addSubview(subview1)
+        view.addSubview(subview2)
         
 
         let scheduler = TestScheduler(initialClock: 0)
@@ -45,6 +55,9 @@ class RxResponderChainTests: XCTestCase {
         let observerForView = scheduler.createObserver(TestEvent.self)
         let observerForVC = scheduler.createObserver(TestEvent.self)
 
+        let observerForSubview = scheduler.createObserver(TestEvent.self)
+        let observerForTestEvent2 = scheduler.createObserver(TestEvent2.self)
+
         scheduler.scheduleAt(0) {
             view.rx.responderChain.event(TestEvent.self)
                 .subscribe(observerForView)
@@ -52,6 +65,14 @@ class RxResponderChainTests: XCTestCase {
 
             viewController.rx.responderChain.event(TestEvent.self)
                 .subscribe(observerForVC)
+                .addDisposableTo(self.disposeBag)
+
+            subview2.rx.responderChain.event(TestEvent.self)
+                .subscribe(observerForSubview)
+                .addDisposableTo(self.disposeBag)
+
+            view.rx.responderChain.event(TestEvent2.self)
+                .subscribe(observerForTestEvent2)
                 .addDisposableTo(self.disposeBag)
 
             xs.bindTo(viewController.view.rx.responderChain)
@@ -69,6 +90,9 @@ class RxResponderChainTests: XCTestCase {
 
         XCTAssertEqual(observerForView.events, expectedEvent)
         XCTAssertEqual(observerForVC.events, expectedEvent)
+        XCTAssertEqual(observerForSubview.events, [])
+        XCTAssertEqual(observerForTestEvent2.events, [])
+
     }
 #endif
 }
